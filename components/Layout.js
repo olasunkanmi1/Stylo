ccd31b72d8b6c975f253b912b749ac5e3f5bde70
Olasunkanmi <abdulsalamquadri999@gmail.com>
2022-12-27 21:36:19 +0100

    designed property, cart and login page and set up context and cookies

---
+++ components/Layout.js
@@ -0,0 +1,56 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import { useContext, useEffect, useState } from 'react';
+import { Store } from '../utils/Store';
+
+const Layout = ({ title, children }) => {
+  const { state } = useContext(Store);
+  const { cart } = state;
+
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col justify-between ">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/" className="text-lg font-bold">
+              {' '}
+              amazona{' '}
+            </Link>
+
+            <div>
+              <Link href="/cart" className="p-2">
+                Cart
+                {cartItemsCount > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cartItemsCount}
+                  </span>
+                )}
+              </Link>
+              <Link href="/login" className="p-2">
+                {' '}
+                Login{' '}
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2022 Amazona</p>
+        </footer>
+      </div>
+    </>
+  );
+};
+export default Layout;
